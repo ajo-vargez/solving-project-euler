a427897b557d5ccc4c5b472703ddab371b79ff97
Ajo <ajo@jupitersupplystation.com>
2020-06-29 14:14:36 +0530

    Solve problem one

---
+++ multiplesOfThreeAndFive.js
@@ -0,0 +1,13 @@
+"use strict"
+
+function multiplesOfThreeAndFive(upto) {
+    let sum = 0;
+    for (let i = 0; i < upto; i++) {
+        if ((i % 3 === 0) || (i % 5 === 0)) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+console.log(multiplesOfThreeAndFive(1000));

c39474452c414caf360283ed25f38a3a89778d01
Ajo <ajo@jupitersupplystation.com>
2020-06-30 14:47:19 +0530

    Solve problem 3

--- problems/3-largestPrimeFactor.js
+++ problems/3-largestPrimeFactor.js
@@ -1,2 +1,25 @@
 "use strict";
 
+
+// Find the prime factor of a number.
+const theNumber = 600851475143;
+
+
+/**
+ * Find the prime factors.
+ * 
+ * @param {number} theNumber - The number whose largest prime factors are to be found.
+ */
+function findPrimeFactorsOf(theNumber) {
+    let n = theNumber;
+    // Start i from 2 since any number divided by 1 is the same number.
+    for (let i = 2; i < Math.sqrt(n); i++){
+        if ((n % i) === 0) {
+            n = n / i;
+        }
+    }
+    return n;
+}
+
+
+console.log("The largest prime factor of " + theNumber + " is = " + findPrimeFactorsOf(theNumber));

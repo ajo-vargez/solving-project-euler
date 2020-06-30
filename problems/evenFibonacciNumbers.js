2972fe294461e81f15410a727e2bc2bd26ab06b7
Ajo <ajo@jupitersupplystation.com>
2020-06-30 12:28:00 +0530

    Add some documentation

--- problems/evenFibonacciNumbers.js
+++ problems/evenFibonacciNumbers.js
@@ -1,7 +1,7 @@
 "use strict";
 
 
-// The limit upto which we need to find the fibonacci terms.
+// The limit upto which we need to find the fibonacci terms. 4 million.
 const upto = 4000000;
 
 
@@ -9,19 +9,27 @@ const upto = 4000000;
  * Find fibonacci terms upto a certain limit.
  * 
  * @param {number} upperbound - The number upto which fibonacci terms need to be found.
+ * @returns - The array of fibonacci terms.
  */
 function getFibonacciTerm(upperbound) {
+    // Initialise the fibonacci terms starting from 1 and 2.
     let fibonacciTerm = [1, 2];
+
+    // Start the iteration from 2 since we're starting from 1 and 2.
     let i = 2;
+
+    // Iterate untill you get all the fibonacci terms which are less than the upperbound.
     while (true) {
-		let n = fibonacciTerm[i - 1] + fibonacciTerm[i - 2];
+        let n = fibonacciTerm[i - 1] + fibonacciTerm[i - 2];
+        
+        // If the fibonacci term is greater than the upperbound which is 4 million, don't add it to
+        // the array.
 		if (n > upperbound) {
 			break;
 		}
         fibonacciTerm[i] = n;
         i++;
 	}
-    console.log(fibonacciTerm);
     return fibonacciTerm;
 }
 
@@ -30,15 +38,17 @@ function getFibonacciTerm(upperbound) {
  * Find the sum of even fibonacci numbers from the input array.
  * 
  * @param {number[]} fibonacciTerm - An array of fibonacci terms less than 4 million.
+ * @returns - The sum of even fibonacci numbers.
  */
 function findSumOfEvenFibonacciNumbers(fibonacciTerm) {
+    // Initialise the sum.
     let sum = 0;
     for (let i = 0; i < fibonacciTerm.length; i++) {
+        // Check if the fibonacci term is an even number. If so, add it to the sum.
         if ((fibonacciTerm[i] % 2) === 0) {
             sum += fibonacciTerm[i];
         }
     }
-    console.log(sum);
     return sum;
 }
 
--- problems/multiplesOfThreeAndFive.js
+++ problems/multiplesOfThreeAndFive.js
@@ -2,13 +2,16 @@
 
 
 /**
+ * Find the sum of all the multiples of 3 and 5 upto 1000.
  * 
  * @param {Number} upto - The number upto which we need to find the multiples of 3 & 5.
- * @returns - The sum of all the multiples of 3 & 5 from 0 to 'upto'.
+ * @returns - The sum of all the multiples of 3 & 5.
  */
-function multiplesOfThreeAndFive(upto) {
+function findSumOfMultiplesOfThreeAndFive(upto) {
+    // Initialise the sum.
     let sum = 0;
     for (let i = 0; i < upto; i++) {
+        // Check if the number is divisible by 3 or 5. If so, add it to the sum.
         if ((i % 3 === 0) || (i % 5 === 0)) {
             sum += i;
         }
@@ -17,5 +20,5 @@ function multiplesOfThreeAndFive(upto) {
 }
 
 
-// call the function with the parameter.
-console.log(multiplesOfThreeAndFive(1000));
+console.log("The sum of all the multiples of 3 and 5 are "
+    + findSumOfMultiplesOfThreeAndFive(1000));

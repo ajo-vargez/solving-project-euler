7c85e979127a5ec54935a48f4bbfc9cf58a91678
Ajo <ajo@jupitersupplystation.com>
2020-06-30 12:12:17 +0530

    Add comments and spaces

--- problems/multiplesOfThreeAndFive.js
+++ problems/multiplesOfThreeAndFive.js
@@ -1,5 +1,11 @@
 "use strict";
 
+
+/**
+ * 
+ * @param {Number} upto - The number upto which we need to find the multiples of 3 & 5.
+ * @returns - The sum of all the multiples of 3 & 5 from 0 to 'upto'.
+ */
 function multiplesOfThreeAndFive(upto) {
     let sum = 0;
     for (let i = 0; i < upto; i++) {
@@ -10,4 +16,6 @@ function multiplesOfThreeAndFive(upto) {
     return sum;
 }
 
+
+// call the function with the parameter.
 console.log(multiplesOfThreeAndFive(1000));

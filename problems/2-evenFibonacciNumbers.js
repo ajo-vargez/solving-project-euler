88d6c8da086fe1c7a88ce12f00b98fa6a058276d
Ajo <ajo@jupitersupplystation.com>
2020-07-04 08:48:46 +0530

    Change some variable names

--- problems/2-evenFibonacciNumbers.js
+++ problems/2-evenFibonacciNumbers.js
@@ -13,45 +13,45 @@ const upto = 4000000;
  */
 function getFibonacciTerms(upperbound) {
     // Initialise the fibonacci terms starting from 1 and 2.
-    let fibonacciTerms = [1, 2];
+    let fibonacciSequence = [1, 2];
 
     // Start the iteration from 2 since we're starting from 1 and 2.
     let i = 2;
 
     // Iterate untill you get all the fibonacci terms which are less than the upperbound.
     while (true) {
-        let n = fibonacciTerms[i - 1] + fibonacciTerms[i - 2];
+        let sumOfTheTwoNumbers = fibonacciSequence[i - 1] + fibonacciSequence[i - 2];
         
         // If the fibonacci term is greater than the upperbound which is 4 million, don't add it to
         // the array.
-		if (n > upperbound) {
-			break;
-		}
-        fibonacciTerms[i] = n;
+        if (sumOfTheTwoNumbers > upperbound) {
+            break;
+        }
+        fibonacciSequence[i] = sumOfTheTwoNumbers;
         i++;
 	}
-    return fibonacciTerms;
+    return fibonacciSequence;
 }
 
 
 /**
  * Find the sum of even fibonacci numbers from the input array.
  * 
- * @param {number[]} fibonacciTerms - An array of fibonacci terms less than 4 million.
+ * @param {number[]} fibonacciSequence - An array of fibonacci terms less than 4 million.
  * @returns - The sum of even fibonacci numbers.
  */
-function findSumOfEvenFibonacciNumbers(fibonacciTerms) {
+const sumOfEvenFibonacciNumbers = (fibonacciSequence) => {
     // Initialise the sum.
     let sumOfEvenFibonacciNumbers = 0;
-    for (let i = 0; i < fibonacciTerms.length; i++) {
+    for (let i = 0; i < fibonacciSequence.length; i++) {
         // Check if the fibonacci term is an even number. If so, add it to the sum.
-        if ((fibonacciTerms[i] % 2) === 0) {
-            sumOfEvenFibonacciNumbers += fibonacciTerms[i];
+        if ((fibonacciSequence[i] % 2) === 0) {
+            sumOfEvenFibonacciNumbers += fibonacciSequence[i];
         }
     }
     return sumOfEvenFibonacciNumbers;
-}
+};
 
 
 console.log("The sum of even fibonacci numbers which are less than " + upto + " are "
-    + findSumOfEvenFibonacciNumbers(getFibonacciTerms(upto)));
+    + sumOfEvenFibonacciNumbers(getFibonacciTerms(upto)));

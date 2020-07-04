da372d516d285029a965dc46e95683dc620ef520
Ajo <ajo@jupitersupplystation.com>
2020-07-04 08:51:59 +0530

    Use arrow function instead of a function

--- problems/1-multiplesOfThreeAndFive.js
+++ problems/1-multiplesOfThreeAndFive.js
@@ -7,22 +7,24 @@ const upperbound = 1000;
 
 /**
  * Find the sum of all the multiples of 3 and 5 upto 1000.
- * 
- * @param {Number} upto - The number upto which we need to find the multiples of 3 & 5.
+ *
+ * @param {Number} upperbound - The number upto which we need to find the multiples of 3 & 5.
  * @returns - The sum of all the multiples of 3 & 5.
  */
-function findSumOfMultiplesOfThreeAndFive(upto) {
-    // Initialise the sum.
-    let sumOfMultiplesOfThreeAndFive = 0;
-    for (let i = 0; i < upto; i++) {
-        // Check if the number is divisible by 3 or 5. If so, add it to the sum.
-        if ((i % 3 === 0) || (i % 5 === 0)) {
-            sumOfMultiplesOfThreeAndFive += i;
-        }
-    }
-    return sumOfMultiplesOfThreeAndFive;
-}
+const sumOfMultiplesOfThreeAndFive = (upperbound) => {
+	// Initialise the sum.
+	let sumOfMultiplesOfThreeAndFive = 0;
+	for (let i = 0; i < upperbound; i++) {
+		// Check if the number is divisible by 3 or 5. If so, add it to the sum.
+		if (i % 3 === 0 || i % 5 === 0) {
+			sumOfMultiplesOfThreeAndFive += i;
+		}
+	}
+	return sumOfMultiplesOfThreeAndFive;
+};
 
 
-console.log("The sum of all the multiples of 3 and 5 are "
-    + findSumOfMultiplesOfThreeAndFive(upperbound));
+console.log(
+	"The sum of all the multiples of 3 and 5 are " +
+		sumOfMultiplesOfThreeAndFive(upperbound)
+);
